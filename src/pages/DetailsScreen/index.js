d6564f78266d7b304e792aecb7e326196375049b
ianabreu <ianabreudev@gmail.com>
2022-12-16 03:41:56 -0300

    Finalizando estilização da página DetailsScreen

--- src/pages/DetailsScreen/index.js
+++ src/pages/DetailsScreen/index.js
@@ -1,39 +1,70 @@
 import React, { useState } from 'react';
-import { Image, Text, View } from 'react-native';
-import {Container, Title, Genres} from './styles';
+import { Dimensions, Image, ScrollView, Text, View } from 'react-native';
+import { Container, Title, Genres, Capa, Overview, DetailTwin, TitleOverview, WatchButton, WatchText, Play, Heart } from './styles';
 
 import StarsRate from '../../components/StarsRate';
 
 export default function DetailsScreen({ route }) {
   const [data, setData] = useState(route?.params?.params);
+  const { width, height } = Dimensions.get('screen')
 
   const convertMinutesInHours = (minuts) => {
-    const hours = Math.floor(minuts/ 60);          
+    const hours = Math.floor(minuts / 60);
     const min = minuts % 60;
-    const textHours = (`00${hours}`).slice(-2);
+    const textHours = (`${hours}`).slice(-2);
     const textMinuts = (`00${min}`).slice(-2);
-    
-    return `${textHours }:${textMinuts}`;
+
+    return `${textHours}h ${textMinuts}m`;
   };
+  function formatingDate(aaaaMMdd) {
+    const arrayDate = aaaaMMdd.split('-');
+    const year = arrayDate[0];
+    const month = arrayDate[1];
+    const day = arrayDate[2];
+    return `${day}/${month}/${year}`;
+  }
+  console.log(data.genres)
 
-  
-return (
+  return (
     <Container>
-      <View>
-        <View>
-          <Image/>
-        </View>
+      <Image resizeMode='cover' source={{ uri: `https://image.tmdb.org/t/p/w500/${data.backdrop_path}` }}
+        style={{
+          width: width,
+          height: height,
+          position: 'absolute',
+          zIndex: -10,
+          top: 0,
+          left: 0,
+          opacity: 0.2
+        }}
+      />
+      <ScrollView>
 
-        <View>
-        <Title>{data.title || data.original_title || data.name || data.original_name}</Title>
-        <Genres>{data.genre_ids}</Genres>
-        <StarsRate rating={data.vote_average}/>
-        <Genres>{data.release_date}</Genres>
-        <Genres>{convertMinutesInHours(Number(data.runtime))}</Genres>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
+          <View style={{ flex: 0.8 }}>
+            <Capa source={{ uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}` }} />
+          </View>
 
+          <View style={{ justifyContent: 'flex-start', alignItems: 'center', flex: 1 }}>
+            <Title>{data.title || data.original_title || data.name || data.original_name}</Title>
+            {data.genres.map((item) => (<Genres key={item.id}>{item.name}</Genres>))}
+            <StarsRate rating={data.vote_average} />
+            <DetailTwin>{data.first_air_date ? formatingDate(data.first_air_date) : formatingDate(data.release_date)}</DetailTwin>
+            {data.runtime && <DetailTwin>{convertMinutesInHours(Number(data.runtime))}</DetailTwin>}
+
+          </View>
         </View>
-      </View>
-        <Text>{data.overview}</Text>
+        <TitleOverview>Sinopse:</TitleOverview>
+        <Overview>{data.overview}</Overview>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-around', marginBottom: 60 }}>
+          <WatchButton>
+            <Play />
+            <WatchText>Trailer</WatchText>
+          </WatchButton>
+          <Heart />
+        </View>
+      </ScrollView>
     </Container>
   );
 }
--- src/pages/DetailsScreen/styles.js
+++ src/pages/DetailsScreen/styles.js
@@ -1,9 +1,70 @@
 import styled from "styled-components/native";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export const Container = styled.ScrollView`
+
+export const Container = styled.SafeAreaView`
+flex: 1;
+background-color: #101210;
+padding: 0 10px;
+`;
+export const Capa = styled.Image`
+width: 165px;
+height: 247.5px;
+border-radius: 7px;
+margin: 5px;
+`;
+export const Title = styled.Text`
+text-align: center;
+font-size: 24px;
+color: #FFF;
+`;
+export const Genres = styled.Text`
+text-align: center;
+color: #FFF;
+font-size: 14px;
+font-weight: 100;
+`;
+export const DetailTwin = styled.Text`
+text-align: center;
+font-size: 18px;
+color: #FFF;
+`;
+export const TitleOverview = styled.Text`
+text-align: center;
+padding-top: 10px;
+font-size: 20px;
+font-weight: bold;
+color: #FFF;
 
 `;
-export const Capa = styled.Image``;
-export const Title = styled.Text``;
-export const Genres = styled.Text``;
-export const Button = styled.TouchableOpacity``;
+export const Overview = styled.Text`
+font-size: 18px;
+line-height: 24px;
+color: #FFF;
+padding-bottom: 20px;
+`;
+export const WatchButton = styled.TouchableOpacity`
+flex-direction: row;
+background-color: #D22730;
+align-items: center;
+border-radius: 10px;
+padding: 10px 20px;
+`; 
+export const WatchText = styled.Text`
+font-size: 24px;
+font-weight: bold;
+color: #FFF;
+`; 
+export const Play = styled(Icon).attrs({
+    name: 'play-circle-outline',
+    size: 30,
+})`
+padding-right: 5px;
+color: #FFF;
+`
+export const Heart = styled(Icon).attrs({
+    name: 'favorite-outline',
+    size: 50,
+})`
+color: #D22730;
+`
