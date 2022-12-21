c9f02188ff2122718ba3ffe48109864bb8f60107
ianabreu <ianabreudev@gmail.com>
2022-12-21 06:23:44 -0300

    resolvendo bugs de falta de informacoes da api e renders condicionais

--- src/pages/DetailsScreen/index.js
+++ src/pages/DetailsScreen/index.js
@@ -23,36 +23,46 @@ export default function DetailsScreen({ route }) {
     const day = arrayDate[2];
     return `${day}/${month}/${year}`;
   }
+  console.log(data.runtime);
 
   return (
     <Container>
+      {data.backdrop_path &&
       <Image resizeMode='cover' source={{ uri: `https://image.tmdb.org/t/p/w500/${data.backdrop_path}` }}
-        style={{
-          width: width,
-          height: height,
-          position: 'absolute',
-          zIndex: -10,
-          top: 0,
-          left: 0,
-          opacity: 0.2
-        }}
+      style={{
+        width: width,
+        height: height,
+        position: 'absolute',
+        zIndex: -10,
+        top: 0,
+        left: 0,
+        opacity: 0.2
+      }}
       />
+    }
       <ScrollView>
 
         <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start' }}>
           <View style={{ flex: 0.8 }}>
-            <Capa source={{ uri: `https://image.tmdb.org/t/p/w500/${data.poster_path}` }} />
+            <Capa source={{ uri: data.poster_path != null ?
+             `https://image.tmdb.org/t/p/w500/${data.poster_path}`
+            :
+            'https://spassodourado.com.br/wp-content/uploads/2015/01/default-placeholder.png'
+            }} />
           </View>
 
           <View style={{ justifyContent: 'flex-start', alignItems: 'center', flex: 1 }}>
-            <Title>{data.title || data.original_title || data.name || data.original_name}</Title>
-            {data.genres.map((item) => (<Genres key={item.id}>{item.name}</Genres>))}
+            <Title>{data.title || data.original_title || data.name || data.original_name || 'Sem Título'}</Title>
+            {data.genres && data.genres.map((item) => (
+            <Genres key={item.id}>{item.name}</Genres>
+            ))}
             <StarsRate rating={data.vote_average} />
-            <DetailTwin>{data.first_air_date ? formatingDate(data.first_air_date) : formatingDate(data.release_date)}</DetailTwin>
-            {data.runtime && <DetailTwin>{convertMinutesInHours(Number(data.runtime))}</DetailTwin>}
+            <DetailTwin>{data.first_air_date ? formatingDate(data.first_air_date) : data.release_date ? formatingDate(data.release_date) : ''}</DetailTwin>
+            {data.runtime != 0 && (<DetailTwin>{convertMinutesInHours(Number(data.runtime))}</DetailTwin>)}
 
           </View>
         </View>
+
         <TitleOverview>Sinopse:</TitleOverview>
         <Overview>
           {data.overview === '' ? 'Não temos uma sinopse em Português do Brasil.'
